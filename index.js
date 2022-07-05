7435df5a052be3bcfe338ae8b74ae1ed3f1663d3
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-05 23:57:13 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import express from 'express'
+const express = require('express')
 const app = express()
+const data = require('./vnlapi.json')
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/api', (req, res) => {
+  res.send(data)
 })
 
 app.listen(port, () => {
