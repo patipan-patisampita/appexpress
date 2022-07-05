4537041b6cbf54ecc7341c29e9465d1206d1a627
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-06 00:08:07 +0700

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const data = require('./vnlapi.json')
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!ok')
+  res.send('Hello World!')
 })
 app.get('/api', (req, res) => {
   res.send(data)
